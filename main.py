ad282554373e4a74af9f6b9ec6cc6879d976cfc0
statelinecat <statelinecat@gmail.com>
2024-08-12 02:06:58 +0300

    add code

---
+++ main.py
@@ -0,0 +1,41 @@
+import requests
+import pprint
+
+print('ДЗ. Задача 1.')
+
+params1 = {
+    'q': 'html'
+}
+
+response = requests.get('https://api.github.com/search/repositories', params=params1)
+print(response.status_code)
+response_json = response.json()
+pprint.pprint(response_json)
+
+
+print('ДЗ. Задача 2.')
+
+params2 = {
+    'userId': '1',
+}
+
+response = requests.get('https://jsonplaceholder.typicode.com/posts', params=params2)
+print(response.status_code)
+response_json = response.json()
+pprint.pprint(response_json)
+
+
+print('ДЗ. Задача 3.')
+
+data = {
+    'title': 'foo',
+    'body': 'bar',
+    'userId': 1
+}
+
+response = requests.post('https://jsonplaceholder.typicode.com/posts', data=data)
+print(response.status_code)
+response_json = response.json()
+print(response_json)
+
+
